24105b15714bfec37989ed5c5b6e60f572253019
Aloxaf <aloxafx@gmail.com>
2026-06-04 13:42:55 +0800

    fix: crash on ffmpeg -i
    close #176

--- fzf-tab.zsh
+++ fzf-tab.zsh
@@ -138,7 +138,13 @@ builtin unalias -m '[^+]*'
 
   # must run with user options; don't move `emulate -L zsh` above this line
   (( $+builtins[fzf-tab-compcap-generate] )) && fzf-tab-compcap-generate -i
-  COLUMNS=500 _ftb__main_complete "$@" || ret=$?
+  # Work around zsh's non-reentrant static pattern buffer.  SIGCHLD can refresh
+  # ZLE while a completer is matching a parameter pattern; prompt expansion
+  # during that refresh can overwrite the active pattern.
+  () {
+    setopt localoptions no_monitor no_notify
+    COLUMNS=500 _ftb__main_complete "$@"
+  } "$@" || ret=$?
   (( $+builtins[fzf-tab-compcap-generate] )) && fzf-tab-compcap-generate -o
 
   emulate -L zsh -o extended_glob
